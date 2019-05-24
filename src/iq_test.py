ba227f1a1b1749e5bd1318c058dc968b46e5365e
Lukman Edwindra <lukman.edwindra@taralite.com>
2019-05-24 11:04:43 +0700

    added .py script

--- src/iq_test.py
+++ src/iq_test.py
@@ -2,9 +2,13 @@ def iq_test(numbers):
     """
     Description:
 
-    Bob is preparing to pass IQ test. The most frequent task in this test is to find out which one of the given numbers differs from the others. Bob observed that one number usually differs from the others in evenness. Help Bob — to check his answers, he needs a program that among the given numbers finds one that is different in evenness, and return a position of this number.
+    Bob is preparing to pass IQ test. The most frequent task in this test is to find out which one of the given 
+    numbers differs from the others. Bob observed that one number usually differs from the others in evenness. Help 
+    Bob — to check his answers, he needs a program that among the given numbers finds one that is different in 
+    evenness, and return a position of this number.
 
-    ! Keep in mind that your task is to help Bob solve a real IQ test, which means indexes of the elements start from 1 (not 0)
+    ! Keep in mind that your task is to help Bob solve a real IQ test, which means indexes of the elements start from 
+    1 (not 0)
 
     Examples:
     iq_test("2 4 7 8 10") => 3 // Third number is odd, while the rest of the numbers are even

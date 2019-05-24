5f3d6a9ecbc396764916c15f60d0921460e2b501
Lukman Edwindra <lukman.edwindra@taralite.com>
2019-05-24 15:17:16 +0700

    added things

--- src/persistent_bugger.py
+++ src/persistent_bugger.py
@@ -2,7 +2,8 @@ def persistence(n):
     """
     Description:
 
-    Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+    Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, 
+    which is the number of times you must multiply the digits in num until you reach a single digit.
 
     Examples:
     persistence(39) => 3v# Because 3*9 = 27, 2*7 = 14, 1*4=4 # and 4 has only one digit.

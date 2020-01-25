c88c29866d73f2b96daeeed2c733433793da0bcc
ledwindra <lukman.edwindra@gmail.com>
2020-01-26 00:45:43 +0700

    add stuff

--- kyu-5/pagination-helper/pagination_helper.py
+++ kyu-5/pagination-helper/pagination_helper.py
@@ -12,39 +12,45 @@ class PaginationHelper:
     def page_count(self):
 
         i = 0
-        array = self.collection
+        collection = self.collection
         item_per_page = self.item_per_page
 
-        if len(self.collection) == self.item_per_page:
+        if len(collection) == item_per_page:
             return 1
         else:
-            while len(array[(item_per_page * i) : (item_per_page * (i + 1))]) > 0:
+            while len(collection[(item_per_page * i) : (item_per_page * (i + 1))]) > 0:
                 i += 1
 
         return i
     
     def page_item_count(self, page_index):
 
+        page_count = self.page_count()
+        item_per_page = self.item_per_page
+        collection = self.collection
         
-        if page_index >= self.page_count():
+        if page_index >= page_count:
             return -1
         elif page_index == 0:
-            return self.item_per_page
+            return item_per_page
         else:
-            return len(self.collection[self.item_per_page - (page_index - 1) : self.item_per_page * (page_index + 1)])
+            return len(collection[item_per_page * page_index : item_per_page * (page_index + 1)])
 
     def page_index(self, item_index):
 
-        array = self.collection
+        collection = self.collection
         item_per_page = self.item_per_page
         item_count = self.item_count()
 
-        i = 0
-        if item_index < 0 or item_index > item_count:
+        try:            
+            i = 0
+            if item_index < 0 or item_index > item_count:
+                return -1
+            elif collection[item_index] in collection[ : item_per_page]:
+                return 0
+            else:
+                while not collection[item_index] in collection[(item_per_page * i) : item_per_page * (i + 1)]:
+                    i += 1
+                return i
+        except IndexError:
             return -1
-        elif array[item_index] in array[ : item_per_page]:
-            return 0
-        else:
-            while not array[item_index] in array[(item_per_page * i) : item_per_page * (i + 1)]:
-                i += 1
-            return i

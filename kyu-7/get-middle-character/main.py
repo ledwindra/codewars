ce19cb2fd83c51a7891ebbe19104488c687fa3e4
ledwindra <lukman.edwindra@gmail.com>
2020-01-26 19:36:07 +0700

    added kyu 7

--- kyu-7/get-middle-character/main.py
+++
@@ -1,8 +0,0 @@
-def get_middle(s):
-    
-    odd = int((len(s) + 1) / 2)
-    even = int(len(s) / 2)
-    
-    if len(s) % 2 != 0:
-        return s[odd - 1]
-    return s[even - 1 : even + 1]
---
+++ python/kyu-7/vowel-count/test_vowel_count.py
@@ -0,0 +1,6 @@
+from vowel_count import get_count
+
+class TestVowelCount:
+
+    def test_0(self):
+        assert get_count("abracadabra") == 5

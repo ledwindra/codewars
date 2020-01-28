1ed3550a921fa3d673c4c4dbf773439c30750904
ledwindra <lukman.edwindra@gmail.com>
2020-01-28 22:39:03 +0700

    added kata from kyu 6

--- python/kyu-6/persistent-bugger/persistent_bugger.py
+++ python/kyu-6/persistent-bugger/persistent_bugger.py
@@ -1,35 +1,12 @@
-def persistence(n=None):
-    """
-    Description:
+def persistence(n):
 
-    Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, 
-    which is the number of times you must multiply the digits in num until you reach a single digit.
-
-    Examples:
-    persistence(39) => 3v# Because 3*9 = 27, 2*7 = 14, 1*4=4 # and 4 has only one digit.
-
-    persistence(999) => 4 # Because 9*9*9 = 729, 7*2*9 = 126, # 1*2*6 = 12, and finally 1*2 = 2.
-
-    persistence(4) => 0 # Because 4 is already a one-digit number.    
-    """
-    
     step = 0
-    
     while n >= 10:
-
         multiplier = 1
-        nlist = list(str(n))
-
-        for i in range(len(nlist)):
-            nlist[i] = int(nlist[i])
-
-        for i in nlist:
-            multiplier = multiplier * i
+        array = [int(x) for x in str(n)]
+        for i in array:
+            multiplier *= i
             n = multiplier
-
         step += 1
-    
-    return step
 
-if __name__ == "__main__":
-    persistence()    
+    return step

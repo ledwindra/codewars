28d8a8512d1c9514f80d67e124e6cd959fab12c2
ledwindra <lukman.edwindra@gmail.com>
2020-01-31 16:26:35 +0700

    Added Ruby

---
+++ ruby/kyu-8/even-or-odd/tc_even_or_odd.rb
@@ -0,0 +1,14 @@
+require_relative "even_or_odd"
+require "test/unit"
+ 
+class TestSimpleNumber < Test::Unit::TestCase
+ 
+  def test_even_odd
+    assert_equal("Even", even_or_odd(2))
+    assert_equal("Even", even_or_odd(0))
+    assert_equal("Odd", even_or_odd(7))
+    assert_equal("Odd", even_or_odd(1))
+    assert_equal("Odd", even_or_odd(-1))
+  end
+ 
+end

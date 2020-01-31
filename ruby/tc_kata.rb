ed9d00aceb5f9edf63aeea11815e9caad1e364e9
ledwindra <lukman.edwindra@gmail.com>
2020-01-31 18:36:58 +0700

    Added kata from kyu-8 for Ruby

--- ruby/tc_kata.rb
+++ ruby/tc_kata.rb
@@ -1,23 +1,23 @@
-require_relative "./kyu-8/even-or-odd/even_or_odd"
-require_relative "./kyu-8/another-ruby/another_ruby"
 require "test/unit"
+require_relative "./kyu-8/even-or-odd/even_or_odd"
+require_relative "./kyu-8/sum-positive-number/sum_positive_number"
  
-class TestSimpleNumber < Test::Unit::TestCase
+class TestEvenOdd < Test::Unit::TestCase
  
   def test_even_odd
-    assert_equal("Even", even_or_odd(2))
-    assert_equal("Even", even_or_odd(0))
-    assert_equal("Odd", even_or_odd(7))
-    assert_equal("Odd", even_or_odd(1))
-    assert_equal("Odd", even_or_odd(-1))
-  end
+    assert_equal(even_or_odd(2), "Even")
+    assert_equal(even_or_odd(0), "Even")
+    assert_equal(even_or_odd(7), "Odd")
+    assert_equal(even_or_odd(1), "Odd")
+    assert_equal(even_or_odd(-1), "Odd")
+  end 
+end
 
-  def test_another_ruby
-    assert_equal("Even", another_ruby(2))
-    assert_equal("Even", another_ruby(0))
-    assert_equal("Odd", another_ruby(7))
-    assert_equal("Odd", another_ruby(1))
-    assert_equal("Odd", another_ruby(-1))
-  end
+class TestSumPositive < Test::Unit::TestCase
  
+  def test_sum_positive
+    assert_equal(positive_sum([1, 2, 3, 4, 5]), 15)
+    assert_equal(positive_sum([1,-2,3,4,5]), 13)
+    assert_equal(positive_sum([-1,2,3,4,-5]), 9)
+  end
 end
